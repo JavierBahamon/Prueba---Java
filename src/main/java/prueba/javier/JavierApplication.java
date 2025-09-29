d35175b052264603db43fb9f45cce609d7fde8c6
Javier Bahamon <javierbahamon10@gmail.com>
2025-09-28 23:43:36 -0500

    priemro

---
+++ src/main/java/prueba/javier/JavierApplication.java
@@ -0,0 +1,13 @@
+package prueba.javier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prueba/javier/JavierApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.javier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
